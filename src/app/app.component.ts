cc64cca8133d904c214722795f89e8a730ce377f
Itoro Udoh <aityworld@gmail.com>
2022-11-02 22:45:11 +0000

    :construction: work in progres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,13 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class AppComponent {
-  title = 'icebot';
+  title = 'Ddcoaching';
   images = [
     {title: 'First Slide', short: 'First Slide Short', src: "https://picsum.photos/id/700/900/500"},
     {title: 'Second Slide', short: 'Second Slide Short', src: "https://picsum.photos/id/1011/900/500"},
     {title: 'Third Slide', short: 'Third Slide Short', src: "https://picsum.photos/id/984/900/500"}
   ];
-   
+
   constructor(config: NgbCarouselConfig) {
     config.interval = 2000;
     config.keyboard = true;
